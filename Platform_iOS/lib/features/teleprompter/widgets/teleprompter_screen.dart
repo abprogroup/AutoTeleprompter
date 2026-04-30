2d01f53ead5cbfc78202384caa3144b64df0b8a4
abprogroup <amit@abpro.group>
2026-04-30 20:50:32 +0300

    Fix iOS build: import ScrollDirection for stopped-browsing handler

--- Platform_iOS/lib/features/teleprompter/widgets/teleprompter_screen.dart
+++ Platform_iOS/lib/features/teleprompter/widgets/teleprompter_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show ScrollDirection;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_animate/flutter_animate.dart';

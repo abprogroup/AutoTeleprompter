8cbd8dabf06e851993bc66cb542e726defbf9e74
abprogroup <abmpro.office@gmail.com>
2026-06-22 01:31:47 +0300

    test(ios): cover repeated editor font changes

--- Platform_iOS/test/script/editor_font_service_test.dart
+++ Platform_iOS/test/script/editor_font_service_test.dart
@@ -1,5 +1,7 @@
 import 'package:autoteleprompter/features/script/services/editor_font_service.dart';
 import 'package:autoteleprompter/features/script/services/markup_decoration_service.dart';
+import 'package:autoteleprompter/features/script/widgets/editor/markup_controller.dart';
+import 'package:autoteleprompter/features/script/widgets/editor/styling_logic_mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -40,4 +42,77 @@ void main() {
     expect(children.last.text, ' world');
     expect(children.last.style!.fontFamily, 'Inter');
   });
+
+  testWidgets('font command can reapply selected text repeatedly',
+      (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: _StylingHarness()));
+    final state =
+        tester.state<_StylingHarnessState>(find.byType(_StylingHarness));
+
+    final controller = MarkupController(text: 'Hello world')
+      ..selection = const TextSelection(baseOffset: 0, extentOffset: 5);
+    state.attachControllers([controller]);
+
+    state.applyInlineProperty(
+      'font',
+      '[font=Lora]',
+      '[/font]',
+      skipHistory: true,
+    );
+    expect(controller.text, '[font=Lora]Hello[/font] world');
+    expect(controller.selection.textInside(controller.text), 'Hello');
+
+    state.applyInlineProperty(
+      'font',
+      '[font=Roboto]',
+      '[/font]',
+      skipHistory: true,
+    );
+    expect(controller.text, '[font=Roboto]Hello[/font] world');
+    expect(controller.selection.textInside(controller.text), 'Hello');
+  });
+}
+
+class _StylingHarness extends StatefulWidget {
+  const _StylingHarness();
+
+  @override
+  State<_StylingHarness> createState() => _StylingHarnessState();
+}
+
+class _StylingHarnessState extends State<_StylingHarness>
+    with StylingLogicMixin<_StylingHarness> {
+  List<MarkupController> _controllers = [];
+  MarkupController? _activeController;
+  bool _isGlobalSelection = false;
+  bool _isCleaning = false;
+
+  void attachControllers(List<MarkupController> controllers) {
+    _controllers = controllers;
+    _activeController = controllers.isEmpty ? null : controllers.first;
+  }
+
+  @override
+  List<MarkupController> get controllers => _controllers;
+
+  @override
+  MarkupController? get activeController => _activeController;
+
+  @override
+  bool get isGlobalSelection => _isGlobalSelection;
+
+  @override
+  set isGlobalSelection(bool value) => _isGlobalSelection = value;
+
+  @override
+  bool get isCleaning => _isCleaning;
+
+  @override
+  set isCleaning(bool value) => _isCleaning = value;
+
+  @override
+  void saveHistory({required String description, bool debounce = true}) {}
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
 }
